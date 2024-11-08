771e7196ae18e6c9b495c69e24f545ec1905bdc6
wiguna77 <44219532+wiguna77@users.noreply.github.com>
2024-11-08 14:29:56 +0800

    fixs

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,8 @@
 "use client"
+import { LucideXCircle, TwitterIcon, XCircle, XIcon } from 'lucide-react';
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
+
 
 export function AboutSection() {
   const { ref: aboutRef, inView: aboutInView } = useInView({
@@ -44,14 +46,86 @@ export function AboutSection() {
             communityInView ? 'opacity-100' : 'opacity-0'
           }`}
         >
-          <div className="w-1/2 p-6 border border-white bg-black bg-opacity-50 rounded-lg mr-5">
-            <h3 className="text-2xl font-semibold mb-2 text-white">Community</h3>
-            <p className="text-gray-300">Join our community and become part of the movement!</p>
-          </div>
-          <div className="w-1/2 p-6 text-right border border-white bg-black bg-opacity-50 rounded-lg ml-5">
-            <h3 className="text-2xl font-semibold mb-2 text-white">Buy $PEOW</h3>
-            <p className="text-gray-300">Buy $PEOW now and be a part of our growing ecosystem!</p>
-          </div>
+<div className="flex flex-col md:flex-row md:space-x-5 space-y-5 md:space-y-0" id='community'>
+  <div className="w-full md:w-1/2 p-6 text-center md:text-right border border-white bg-black bg-opacity-50 rounded-lg">
+    <h3 className="text-2xl font-semibold mb-2 text-white">Community</h3>
+    <p className="text-gray-300">Join our community and become part of the movement!</p>
+
+    {/* Flex container for links */}
+    <div className="flex space-x-4 mt-4 justify-center md:justify-end flex-wrap">
+      {/* X (formerly Twitter) Icon */}
+      <a href="http://x.com/peowsolana" target='_blank' rel="noopener noreferrer">
+      <div className="rounded-full bg-black p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" fill="white">
+          <path d="M23.643 4.937a9.727 9.727 0 0 1-2.828.775 4.926 4.926 0 0 0 2.165-2.717 9.747 9.747 0 0 1-3.103 1.184 4.904 4.904 0 0 0-8.372 4.47A13.925 13.925 0 0 1 1.671 3.149a4.904 4.904 0 0 0 1.516 6.544 4.868 4.868 0 0 1-2.22-.616v.061a4.906 4.906 0 0 0 3.933 4.808 4.9 4.9 0 0 1-2.212.084 4.906 4.906 0 0 0 4.587 3.413A9.824 9.824 0 0 1 1.2 19.293a13.816 13.816 0 0 0 7.548 2.213c9.055 0 14.01-7.496 14.01-13.985 0-.21-.004-.42-.013-.63a9.936 9.936 0 0 0 2.438-2.546z" />
+        </svg>
+        </div>
+      </a>
+
+      {/* Telegram Icon */}
+      <a href="http://t.me/peowsolana" target='_blank' rel="noopener noreferrer">
+      <div className="rounded-full bg-black p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" width="40" fill="white">
+          <path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z" />
+        </svg>
+        </div>
+      </a>
+      
+    </div>
+  </div>
+
+  <div className="w-full md:w-1/2 p-6 text-center md:text-right border border-white bg-black bg-opacity-50 rounded-lg" id='buy'>
+    <h3 className="text-2xl font-semibold mb-2 text-white">Buy $PEOW</h3>
+    <p className="text-gray-300">Buy $PEOW now and be a part of our growing ecosystem!</p>
+
+    <div className="flex justify-center md:justify-end flex-wrap space-x-2 md:space-x-4 mt-4">
+      {/* BirdEye */}
+      <a href="https://birdeye.so/token/7DptUgc1aRY6geK4WnauRKTTfVFWmLsBWP275sD2Bg9r?chain=solana" target='_blank' rel="noopener noreferrer">
+        <div className="rounded-full bg-black p-2">
+          <img src="/ico/icon-birdeye.svg" width={50} alt="BirdEye"/>
+        </div>
+      </a>
+        {/* Jupiter Aggregator (jup.ag) */}
+        <a href="https://jup.ag/swap/SOL-7DptUgc1aRY6geK4WnauRKTTfVFWmLsBWP275sD2Bg9r" target='_blank' rel="noopener noreferrer">
+            <div className="rounded-full bg-black p-2">
+                <img src="/ico/icon-jupiter.svg" width={50} alt="Jupiter"/>
+            </div>
+        </a>
+
+        {/* DexScreener */}
+        <a href="https://dexscreener.com/solana/ds2cv9r9yrnk52atty7nwybrsxtnjvsl4gayuzh9hkcm" target='_blank' rel="noopener noreferrer">
+            <div className="rounded-full bg-black p-2">
+                <img src="/ico/icon-dexscreener.svg" width={50} alt="DexScreener"/>
+            </div>
+        </a>
+
+        {/* DEXTools */}
+        <a href="https://www.dextools.io/app/en/solana/pair-explorer/DS2cv9R9Yrnk52AttY7nWYbRSxtnJVSL4GAYuzH9hKCm?t=1731045130923" target='_blank' rel="noopener noreferrer">
+            <div className="rounded-full bg-black p-2">
+                <img src="/ico/icon-dextools.svg" width={50} alt="DEXTools"/>
+            </div>
+        </a>
+
+        {/* Meteora */}
+        <a href="https://app.meteora.ag/pools/AjDkGYgQKEpMSvQwNoBMqyWTzDrD2pfGtffTFbGPuYc2" target='_blank' rel="noopener noreferrer">
+            <div className="rounded-full bg-black p-2">
+                <img src="/ico/icon-meteora-grey.svg" width={50} alt="Meteora"/>
+            </div>
+        </a>
+
+        {/* GeckoTerminal */}
+        <a href="https://www.geckoterminal.com/solana/pools/AjDkGYgQKEpMSvQwNoBMqyWTzDrD2pfGtffTFbGPuYc2" target='_blank' rel="noopener noreferrer">
+            <div className="rounded-full bg-black p-2">
+                <img src="/ico/icon-geckoterminal.svg" width={50} alt="GeckoTerminal"/>
+            </div>
+        </a>
+    </div>
+  </div>
+</div>
+
+
+
+
         </section>
 
         {/* Quote and Image Section */}
--- components/home.tsx
+++ components/home.tsx
@@ -38,6 +38,7 @@ export function HomeSection(){
         <div
             className="relative min-h-screen bg-cover bg-center flex items-center justify-center overflow-hidden"
             style={{ backgroundImage: `url('./background/background1.jpg')` }}
+            id='home'
         >
             {/* Main Content */}
             <main className="flex flex-col items-center justify-center text-center text-white px-4 fade-in">
@@ -47,12 +48,12 @@ export function HomeSection(){
 
                 {/* Desktop Buttons */}
                 <div className="hidden md:flex space-x-4">
-                    <button className="bg-[#ff00ec] hover:bg-[#b43ab2] text-white font-bold py-2 px-6 rounded-lg">BUY NOW</button>
+                    <button className="bg-[#ff00ec] hover:bg-[#b43ab2] text-white font-bold py-2 px-6 rounded-lg">BUY NOW <span>💰</span></button>
                     <button className="bg-[#ff00ec] hover:bg-[#b43ab2] text-white font-bold py-2 px-6 rounded-lg">CHART 📊</button>
                 </div>
 
                 {/* Mobile Links - Visible on smaller screens */}
-                <div className="flex flex-col space-y-4 mt-6 md:hidden">
+                <div className="flex flex-col space-y-4 mt-6 md:hidden text-2xl">
                     <h3 className="text-2xl font-bold underline">Useful Links:</h3>
                     <div className="grid grid-cols-2 gap-4 mt-6 md:hidden">
                         <button className="bg-[#ff00ec] hover:bg-[#cc00ff] text-white font-bold py-2 px-6 rounded-lg flex justify-between items-center">
@@ -62,10 +63,14 @@ export function HomeSection(){
                             Chart <span>📊</span>
                         </button>
                         <button className="bg-[#ff00ec] hover:bg-[#b43ab2] text-white font-bold py-2 px-6 rounded-lg flex justify-between items-center">
-                            Twitter <span>🐦</span>
+                            Twitter <span>        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" fill="white">
+          <path d="M23.643 4.937a9.727 9.727 0 0 1-2.828.775 4.926 4.926 0 0 0 2.165-2.717 9.747 9.747 0 0 1-3.103 1.184 4.904 4.904 0 0 0-8.372 4.47A13.925 13.925 0 0 1 1.671 3.149a4.904 4.904 0 0 0 1.516 6.544 4.868 4.868 0 0 1-2.22-.616v.061a4.906 4.906 0 0 0 3.933 4.808 4.9 4.9 0 0 1-2.212.084 4.906 4.906 0 0 0 4.587 3.413A9.824 9.824 0 0 1 1.2 19.293a13.816 13.816 0 0 0 7.548 2.213c9.055 0 14.01-7.496 14.01-13.985 0-.21-.004-.42-.013-.63a9.936 9.936 0 0 0 2.438-2.546z" />
+        </svg></span>
                         </button>
                         <button className="bg-[#ff00ec] hover:bg-[#b43ab2] text-white font-bold py-2 px-6 rounded-lg flex justify-between items-center">
-                            Telegram <span>📨</span>
+                            Telegram <span>        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" width="20" fill="white">
+          <path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z" />
+        </svg></span>
                         </button>
                     </div>
                     <button className="bg-[#ff00ec] hover:bg-[#b43ab2] text-white font-bold py-2 px-6 rounded-lg flex justify-between items-center">
